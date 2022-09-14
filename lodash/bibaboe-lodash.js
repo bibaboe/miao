d34990815f232dfe60405c656a3cfd3551362724
vhwfvs <vhwfvs@qq.com>
2022-09-14 19:38:55 +0800

    difference

--- lodash/bibaboe-lodash.js
+++ lodash/bibaboe-lodash.js
@@ -1,9 +1,21 @@
 var bibaboe = {
+  // 将一个数组里的元素分成【size】个一组
+  chunk: function (array, size = 1) {
+    if (size == 1) return array
+    var result = []
 
-  chunk: function () {
+    for (var i = 0; i < Math.ceil(array.length / size); i++) {
+      var ary = []
+      for (var j = 0; j < size; j++) {
+        ary.push(array[i * size + j])
+      }
+      result.push(ary)
+    }
 
+    return result
   },
 
+  // 删除数组里的false,null,0,'',undefined和NaN
   compact: function (array) {
     var result = []
     for (var i = 0; i < array.length; i++) {
@@ -16,6 +28,7 @@ var bibaboe = {
     return result
   },
 
+    // 把array从start下标开始，不包含end下标的元素替换成value
   fill: function (array, value, start = 0, end = array.length) {
     for (var i = start; i < end; i++) {
       array[i] = value
@@ -23,5 +36,23 @@ var bibaboe = {
     return array
   },
 
+  // 把在values中存在的值排除，返回新数组
+  difference: function (array, values) {
+    var map = {}
+    for (var i = 0; i < values.length; i++) {
+      map[values[i]] = 0
+    }
+
+    var result = []
+    for (var j = 0; j < array.length; j++) {
+      if (!(array[j] in map)) {
+        result.push(array[j])
+      }
+    }
+
+    return result
+  },
+
+  //
 
 }
