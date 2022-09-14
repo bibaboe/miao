6c39f80ecc83ce12feee33d398ce7db62eb14bec
vhwfvs <vhwfvs@qq.com>
2022-09-14 17:37:12 +0800

    1

--- lodash/bibaboe-lodash.js
+++ lodash/bibaboe-lodash.js
@@ -4,6 +4,12 @@ var bibaboe = {
 
   compact: function() {},
 
-  fill: function() {},
+  fill: function (array, value, start = 0, end = array.length) {
+    var result = []
+    for (var i = start; i < end; i++) {
+      result[i] = value
+    }
+    return result
+  },
 
 }
